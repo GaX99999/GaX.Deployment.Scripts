f1d73cf8b820e23f731e52e147655cf481083b64
Jesper Kristensen <jesperkristensen@live.com>
2025-08-09 00:48:28 +0200

    Changes

--- Boot/USB.Boot.ps1
+++ Boot/USB.Boot.ps1
@@ -1,11 +1,11 @@
 [CmdletBinding()]
 param()
 
-#Start the Transcript
+# Start the Transcript
 $Transcript = "$((Get-Date).ToString('yyyy-MM-dd-HHmmss'))-OSDCloud.log"
 $null = Start-Transcript -Path (Join-Path "$env:SystemRoot\Temp" $Transcript) -ErrorAction Ignore
 
-#Load OSDCloud Functions
+# Load OSDCloud Functions
 Invoke-Expression -Command (Invoke-RestMethod -Uri functions.osdcloud.com)
 
 # Check if Secure Boot is enabled
@@ -21,13 +21,39 @@ if (!$secureBoot) {
 
 $disks = Get-WmiObject -Class Win32_LogicalDisk -Filter "DriveType =3"
 If (-not $Disks){
-    Write-Host -ForegroundColor Red  "No disk detected, either the boot image is missing neccesary drivers, or the harddrive needs to be replaced."
+    Write-Host -ForegroundColor Red  "No disk detected, either the boot image is missing necessary drivers, or the hard drive needs to be replaced."
     Start-Sleep -Seconds 300
-    #ToDo - Send warning with model and serial number to teams.
     Exit       
 }
 
 # GaX OSDCloud start script
+
+function Show-Menu {
+    param (
+        $Options
+    )
+    foreach ($key in $Options.Keys) {
+        $menuText = $Options[$key]
+        if ($menuText -match "-") {
+            $parts = $menuText -split "-", 2
+            $prefix = $parts[0].Trim()   # Text before "-"
+            $coloredPart = $parts[1].Trim() # Text after "-"
+
+            if ($coloredPart -match "Standard") {
+                Write-Host "$key. $prefix -" -NoNewline
+                Write-Host " $coloredPart" -ForegroundColor Cyan
+            } elseif ($coloredPart -match "English") {
+            } elseif ($coloredPart -match "Customize") {
+                Write-Host "$key. $prefix -" -NoNewline
+                Write-Host " $coloredPart" -ForegroundColor Green
+            } else {
+                Write-Host "$key. $menuText"
+            }
+        } else {
+            Write-Host "$key. $menuText"
+        }
+    }
+}
 
 $BrandName = 'GaX'
 $BrandColor = '#059297ff'
@@ -37,10 +63,10 @@ $Global:MyOSDCloud = [ordered]@{
     Restart = [bool]$True
     RecoveryPartition = [bool]$true
     OEMActivation = [bool]$true
-	WindowsUpdate = [bool]$false
+    WindowsUpdate = [bool]$false
     WindowsUpdateDrivers = [bool]$false
     WindowsDefenderUpdate = [bool]$false
-	SetTimeZone = [bool]$true
+    SetTimeZone = [bool]$true
     ClearDiskConfirm = [bool]$true
     ShutdownSetupComplete = [bool]$false
     SyncMSUpCatDriverUSB = [bool]$true
@@ -48,15 +74,14 @@ $Global:MyOSDCloud = [ordered]@{
 }
 
 $menuOptions = [ordered]@{
-	"1" = "Windows 11 Pro 24H2 English ";
-    "2" = "Chose between Windows 10,11 and language";
-
+    "1" = "Windows 11 Pro 24H2 English - Standard";
+    "2" = "Choose between Windows 10,11 and language - Customize";
 }
 
 # Display the menu
 Show-Menu -Options $menuOptions
 
-# Funktion til automatisk at formatere brugerens valg
+# Function to automatically format the user's selection
 function Format-Selection {
     param (
         [string]$selection
@@ -65,46 +90,46 @@ function Format-Selection {
 }
 do {
     Show-Menu
-    $selection = Read-Host "Please select option you need"
+    $selection = Read-Host "Please select the option you need"
     
     switch ($selection) {
 
 "1" {
-            Write-Host "You selected: $($menuOptions[$selection])"
-			$OSLanguage = 'en-us'
-			$OSName = 'Windows 11 24H2 x64'
-			$OSEdition = 'Pro'
-			$OSActivation = 'Retail'
-			$arch = $env:PROCESSOR_ARCHITECTURE
+            Write-Host "Selected: $($menuOptions[$selection])"
+            $OSLanguage = 'en-us'
+            $OSName = 'Windows 11 24H2 x64'
+            $OSEdition = 'Pro'
+            $OSActivation = 'Retail'
+            $arch = $env:PROCESSOR_ARCHITECTURE
 
 if ($arch -eq "ARM64") {
     Write-Output "ARM64"
-	    Start-OSDCloudGUIDEV -BrandColor $Brandcolor -BrandName $BrandName
-		#Start-OSDCloudGUIDEV -OSLanguage $OSLanguage -OSActivation $OSActivation -OSName $OSName -OSEdition $OSEdition
+        Start-OSDCloudGUIDEV -BrandColor $Brandcolor -BrandName $BrandName
+        
 } else {
     Write-Output "(x86/x64)"
-	    Start-OSDCloud -OSLanguage $OSLanguage -OSActivation $OSActivation -OSName $OSName -OSEdition $OSEdition
+        Start-OSDCloud -OSLanguage $OSLanguage -OSActivation $OSActivation -OSName $OSName -OSEdition $OSEdition
 }
 
         }
 
 "2" {
-            Write-Host "You selected: $($menuOptions[$selection])"
+            Write-Host "Selected: $($menuOptions[$selection])"
 $arch = $env:PROCESSOR_ARCHITECTURE
 
 if ($arch -eq "ARM64") {
     Write-Output "ARM64"
-	    Start-OSDCloudGUIDEV -BrandColor $Brandcolor -BrandName $BrandName
+        Start-OSDCloudGUIDEV -BrandColor $Brandcolor -BrandName $BrandName
 } else {
     Write-Output "(x86/x64)"
-	    Start-OSDCloudGUI -BrandColor $Brandcolor -BrandName $BrandName
+        Start-OSDCloudGUI -BrandColor $Brandcolor -BrandName $BrandName
 }
 
         }
-		
+        
         #Default option
         default {
-            Write-Host "You selected: $($menuOptions[$selection])"
+            Write-Host "Selected: $($menuOptions[$selection])"
 
         }
     }
@@ -116,16 +141,16 @@ if ($arch -eq "ARM64") {
 
     }
     Else{
-		# Hent processorarkitekturen fra miljøvariablen
+        # Get processor architecture from environment variable
 $arch = $env:PROCESSOR_ARCHITECTURE
 
 if ($arch -eq "ARM64") {
     Write-Output "ARM64"
-	    Start-OSDCloudGUIDEV -BrandColor $Brandcolor -BrandName $BrandName
+        Start-OSDCloudGUIDEV -BrandColor $Brandcolor -BrandName $BrandName
 
 } else {
     Write-Output "(x86/x64)"
-	    Start-OSDCloudGUI -BrandColor $Brandcolor -BrandName $BrandName
+        Start-OSDCloudGUI -BrandColor $Brandcolor -BrandName $BrandName
 }
 
     }
